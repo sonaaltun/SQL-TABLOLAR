750f0764619bdb1849ed43e93f39f72d05069c23
Sona Altun <74629475+sonaaltun@users.noreply.github.com>
2022-05-27 23:11:36 +0300

    Create ödev8-3.sql

---
+++ ödev8-3.sql
@@ -0,0 +1,11 @@
+UPDATE employee
+SET name = 'Ahmet'
+WHERE id = 5;
+
+UPDATE employee
+SET birthday = '2001-01-01'
+WHERE id = 25;
+
+UPDATE employee
+SET email = 'delipop@gmail.com'
+WHERE id = 30
